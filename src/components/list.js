2e8a58a3168ca17a28a3ffdb44086e1e002c0d4e
jtx007 <jamesjacobthomas7@gmail.com>
2019-02-17 01:14:53 -0500

    created skeleton

---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    render() {
+        return (
+            <div className="list">
+                <h1>List</h1>
+            </div>
+        )
+    }
+}
+
+export default List;
